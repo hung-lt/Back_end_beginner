66ba18c519b16dd27ef44d1476d910e7a772ad46
Hưng <92345487+hung-lt@users.noreply.github.com>
2025-04-02 08:59:42 +0700

    Add files via upload

---
+++ Lession01/Lab1.1/Program.cs
@@ -0,0 +1,43 @@
+﻿//Bai 1: Viet chương trinh nhap vao ten av tuoi, sau do
+//in ra màn hình thong bao "Xin chao [ten], ban [tuoi] tuoi!".
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+string? ten;
+int tuoi;
+//Nhạp du lieu tu ban phim
+Console.Write("Nhap ten: ");
+ten = Console.ReadLine();
+Console.Write("nhap tuoi: ");
+tuoi = int.Parse(Console.ReadLine() ?? "0");
+// xuat ra màn hinh
+Console.WriteLine($"Xin chao {ten}, ban {tuoi} tuoi!");
+
+//Bai 2: viêt chuong trinh tinh dien tich hình chữ nhật
+// khi ng dùng nhập chiều dài và chiều rộng
+
+double chieuDai, chieuRong, dienTich;
+try
+{
+    // nhap dữ liệu từ bàn phím
+    Console.Write("Chieu dai: ");
+    chieuDai = double.Parse(Console.ReadLine() ?? "0");
+    Console.Write("Chieu rong: ");
+    chieuRong = double.Parse(Console.ReadLine() ?? "0");
+
+    if (chieuDai <= 0 || chieuRong <= 0)
+        throw new Exception("do dai phai lon hon 0");
+
+    //tinh dien tich
+    dienTich = chieuDai * chieuRong;
+    Console.WriteLine($"dien tich hcn : {dienTich}");
+}
+
+catch (FormatException ex)
+{
+    Console.WriteLine("Loi nhap lieu: " + ex.Message);
+}
+
+catch (Exception ex)
+{
+    Console.WriteLine("Loi " + ex.Message);
+}
---
+++ Lession01/Lab1.3/Tailieu.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1._3
+{
+    class Tailieu
+    {
+        //Khai bao cac thuoc tinh
+        protected string? maTL;
+        protected string? tenNXB;
+        protected int soBan;
+        //Phuong thuc khoi tao mac dinh
+        public Tailieu() {  }
+        //phuong thuc khoi tao co tham so
+        public Tailieu(string maTl, string tenNXB, int soBan)
+        {
+            this.maTL = maTl;
+            this.tenNXB = tenNXB;
+            this.soBan = soBan;
+        }
+
+        // nhap thong tin
+        public virtual void Nhap()
+        {
+            try
+            {
+                Console.Write("+ ma tai lieu: ");
+                maTL = Console.ReadLine();
+                Console.Write("+ ten nha xuat ban: ");
+                tenNXB = Console.ReadLine();
+                Console.Write("+ so ban phat hanh: ");
+                soBan = int.Parse(Console.ReadLine() ?? "0");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        //hien thi thong tin tai lieu
+        public virtual void Xuat()
+        {
+            Console.WriteLine($"+ ma tai lieu: {maTL}");
+            Console.WriteLine($"+ ten nha xuat ban: {tenNXB}");
+            Console.WriteLine($"+ so ban phat hanh: {soBan}");
+        }
+    }
+}
