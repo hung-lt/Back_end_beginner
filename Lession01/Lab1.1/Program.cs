6f68ec551f299c909db24b256129837318de3af8
Hưng <92345487+hung-lt@users.noreply.github.com>
2025-04-07 23:50:41 +0700

    done lab 1.1 & 1.2

--- Lession01/Lab1.1/Program.cs
+++ Lession01/Lab1.1/Program.cs
@@ -1,6 +1,7 @@
-﻿//Bai 1: Viet chương trinh nhap vao ten av tuoi, sau do
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+//Bai 1: Viet chương trinh nhap vao ten av tuoi, sau do
 //in ra màn hình thong bao "Xin chao [ten], ban [tuoi] tuoi!".
-Console.OutputEncoding = System.Text.Encoding.UTF8;
 
 string? ten;
 int tuoi;
@@ -40,4 +41,129 @@ catch (FormatException ex)
 catch (Exception ex)
 {
     Console.WriteLine("Loi " + ex.Message);
+}
+
+//Bai3. Viết chương trình chuyển đổi nhiệt độ từ đọ C sang độ F
+Console.Write("Nhập nhiệt độ (°C): ");
+if (double.TryParse(Console.ReadLine(), out double celsius))
+{
+    double fahrenheit = (celsius * 9 / 5) + 32;
+    Console.WriteLine($"{celsius}°C = {fahrenheit}°F");
+}
+else
+{
+    Console.WriteLine("Vui lòng nhập một số hợp lệ.");
+}
+
+//Bai4. Viết chương trình nhập vào một sô nguyên và kiểm tra xem số đó có phải là số chẵn hay không
+Console.Write("Nhap vao mot so nguyen to: ");
+if(int.TryParse(Console.ReadLine(), out int n))
+{
+    if(n % 2 ==0)
+    {
+        Console.WriteLine($"{n} la mot so chan");
+    }
+    else
+    {
+        Console.WriteLine($"{n} khong phai la so chan");
+    }
+
+}
+else
+{
+    Console.WriteLine("Vui lòng nhập một số hợp lệ.");
+}
+
+
+//Bai5. Viết chương trình tính tổng và tích của 2 số nhập từ bàn phím
+double soMot, soHai, tich, tong;
+    // nhap dữ liệu từ bàn phím
+    Console.Write("nhap so thu nhat: ");
+    soMot = double.Parse(Console.ReadLine() ?? "0");
+    Console.Write("nhap so thu hai: ");
+    soHai = double.Parse(Console.ReadLine() ?? "0");
+
+    //tinh toan
+    tich = soHai * soMot;
+    tong = soMot + soHai;
+    Console.WriteLine($"tong của hai so da nhap : {tong}");
+    Console.WriteLine($"tich của hai so da nhap : {tich}");
+
+//Bai6. Viết chương trình kiểm tra xem một số nhập vào có phải số dương, số âm hay số không
+double? number;
+Console.Write("Nhap vao mot so bat ky: ");
+number = double.Parse(Console.ReadLine() ?? "0");
+
+if(number > 0)
+{
+    Console.WriteLine($"{number} là so duong");
+}
+else if(number < 0){
+    Console.WriteLine($"{number} la so am");
+}
+else
+{
+    Console.WriteLine($"{number} la so khong");
+}
+
+//Bai7. Viết chương trình kiểm tra xem một năm nhập vào có phải năm nhuận hay không 
+int? nam;
+Console.Write("nhap vao mot nam: ");
+nam = int.Parse(Console.ReadLine() ?? "0");
+
+if((nam % 4 == 0 && nam % 100 != 0) || (nam % 400 == 0))
+{
+    Console.WriteLine($"{nam} la nam nhuan");
+}
+else
+{
+    Console.WriteLine($"{nam} khong phai la nam nhuan");
+}
+
+//Bai8. viết chương trình in ra bảng cửu chương từ 1 đến 10
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine($"Bảng cửu chương {i}:");
+    for (int j = 1; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
+
+//Bai9. Viet chương trình tính giai thừa của một số nguyên dương
+int soNguyenDuong;
+Console.Write("Nhap vao mot so nguyen duong: ");
+soNguyenDuong = int.Parse(Console.ReadLine() ?? "0");
+long giaThua = 1;
+for (int i = 1; i<= soNguyenDuong; i++)
+{
+    giaThua *= i;
+}
+Console.WriteLine($"{soNguyenDuong}! = {giaThua}");
+
+//Bai10. viet chương trình kiểm tra xem một số có phải là số nguyên tố hay không
+int num;
+Console.Write("Nhap vao mot so bat ky: ");
+num = int.Parse(Console.ReadLine() ?? "0");
+if(num < 2)
+{
+    Console.WriteLine($"{num} khong phai so nguyen to");
+}
+else
+{
+    bool laSoNguyenTo = true;
+    for (int i = 2; i * i <= num; i++)
+    {
+        if (num % i == 0)
+        {
+            laSoNguyenTo = false;
+            break; // Không cần kiểm tra thêm nữa
+        }
+    }
+
+    if (laSoNguyenTo)
+        Console.WriteLine($"{num} la so nguyen to.");
+    else
+        Console.WriteLine($"{num} khong phai la so nguyen to.");
 }
---
+++ Lession01/Lab1.2/Program.cs
@@ -0,0 +1,395 @@
+﻿using System;
+
+namespace Lab1._2 {
+
+//Bai1: Viet mot hàm để tính tổng của tất cả các so chan trong mọt mảng
+    class Program1
+    {
+        public static void NhapMang(int[] a, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                int value;
+                while (true)
+                {
+                    Console.Write($"a[{i}]: ");
+                    string? input = Console.ReadLine();
+
+                    if (int.TryParse(input, out value))
+                    {
+                        a[i] = value;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Giá trị không hợp lệ. Vui lòng nhập số nguyên.");
+                    }
+                }
+            }
+        }
+
+        public static int TinhTong(int[] a, int n)
+        {
+             int tong= 0;
+             for(int i = 0; i < n; i++)
+             {
+                tong += a[i];
+             }
+             return tong;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            //Khai báo biến n
+            int n;
+            while (true)
+            {
+                Console.Write("Nhập n: ");
+                string? input = Console.ReadLine();
+
+                if (int.TryParse(input, out n) && n > 0)
+                    break;
+                else
+                    Console.WriteLine("Vui lòng nhập một số nguyên dương.");
+            }
+            //Khai báo và khởi tạo mảng số nguyên có n phần tử
+            int[] a = new int[n];
+            //Gọi hàm nhập mảng
+            NhapMang(a, n);
+            //Gọi hàm Tính Tổng các phần tử trong mảng và hiển thị kết quả ra màn hình
+            Console.WriteLine($"Tổng = {TinhTong(a, n)}");
+
+        }
+    }
+
+
+ //Bai 2: Viết chuong trinh nhap  vào mang  gồm n phân tử  nhâp từ bàn phím. Viết hàm để kiểm tra xem mot so có phải so nguyen to khong
+ // hiển thị  chỉ số và giá trị  của những  phần tử là số nguyên tố trong mảng
+    class Program2
+    {
+         public static void NhapMang(int[] a, int n)
+         {
+              for (int i = 0; i < n; i++)
+              {
+                  int value;
+                  while (true)
+                  {
+                      Console.Write($"a[{i}]: ");
+                      string? input = Console.ReadLine();
+
+                      if (int.TryParse(input, out value))
+                      {
+                          a[i] = value;
+                          break;
+                      }
+                      else
+                      {
+                          Console.WriteLine("Giá trị không hợp lệ. Vui lòng nhập số nguyên.");
+                      }
+                  }
+              }
+         }
+         public static bool CheckPrime(int n)
+         {
+              if (n < 2) return false;
+
+              for (int i = 2; i <= Math.Sqrt(n); i++)
+              {
+                  if (n % i == 0)
+                      return false;
+              }
+              return true;
+         }
+         static void Main(string[] args)
+         {
+              Console.OutputEncoding = System.Text.Encoding.UTF8;
+              int n;
+              while (true)
+              {
+                  Console.Write("Nhập n: ");
+                  string? input = Console.ReadLine();
+
+                  if (int.TryParse(input, out n) && n > 0)
+                      break;
+                  else
+                      Console.WriteLine("Vui lòng nhập một số nguyên dương.");
+              }
+              //Khai báo và khởi tạo mảng số nguyên có n phần tử
+              int[] a = new int[n];
+              NhapMang(a, n);
+              Console.WriteLine("Các phần tử là số nguyên tố:");
+              for (int i = 0; i < a.Length; i++)
+              {
+                  if (CheckPrime(a[i]))
+                  {
+                     Console.WriteLine($"Index: {i}, Value: {a[i]}");
+                  }
+              }
+         }
+    }
+   
+//Bai 3: Viet một hàm để đếm số lượng số âm và số dương trong một mảng gồm n phần tử là so nguyen trong mảng
+    class Program3
+    {
+        public static void NhapMang(int[] a, int n)
+        {
+             for (int i = 0; i < n; i++)
+             {
+                  int value;
+                  while (true)
+                  {
+                       Console.Write($"a[{i}]: ");
+                       string? input = Console.ReadLine();
+
+                       if (int.TryParse(input, out value))
+                       {
+                           a[i] = value;
+                           break;
+                       }
+                       else
+                       {
+                           Console.WriteLine("Giá trị không hợp lệ. Vui lòng nhập số nguyên.");
+                       }
+                  }
+             }
+        }
+        public static int DemSoAm(int[] a, int n) 
+        {
+             int soSoAm = 0;
+             for (int i = 0; i < n; i++)
+             {
+                 if (a[i] < 0)
+                 {
+                      soSoAm++;
+                 }
+             }
+             return soSoAm;
+        
+        }
+        public static int DemSoDuong(int[] a, int n)
+        {
+             int soSoDuong = 0;
+              for (int i = 0; i < n; i++)
+              {
+                  if (a[i] > 0)
+                  {
+                      soSoDuong++;
+                  }
+              }
+              return soSoDuong;
+        }
+        static void Main(string[] args)
+        {
+              Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+              int n;
+              while (true)
+              {
+                  Console.Write("Nhập n: ");
+                  string? input = Console.ReadLine();
+
+                  if (int.TryParse(input, out n))
+                      break;
+                  else
+                      Console.WriteLine("Vui lòng nhập một số nguyên.");
+              }
+              //Khai báo và khởi tạo mảng số nguyên có n phần tử
+              int[] a = new int[n];
+              NhapMang(a, n);
+              Console.WriteLine($"Số lượng số âm là : {DemSoAm(a, n)}");
+              Console.WriteLine($"Số lượng số dương là: {DemSoDuong(a,n)}");
+        }
+    }
+
+//Bai 4: Viết hàm để tìm số lớn thứ 2 trong một mảng các số nguyên
+    class Program4
+    {
+        public static void NhapMang(int[] a, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                int value;
+                while (true)
+                {
+                    Console.Write($"a[{i}]: ");
+                    string? input = Console.ReadLine();
+
+                    if (int.TryParse(input, out value))
+                    {
+                        a[i] = value;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Giá trị không hợp lệ. Vui lòng nhập số nguyên.");
+                    }
+                }
+            }
+        }
+        public static int TimSoLonThuHai(int[] a, int n)
+        {
+            if (n < 2)
+            {
+                throw new ArgumentException("Mảng phải có ít nhất 2 phần tử.");
+            }
+
+            int max1 = int.MinValue;
+            int max2 = int.MinValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (a[i] > max1)
+                {
+                    max2 = max1;
+                    max1 = a[i];
+                }
+                else if (a[i] > max2 && a[i] < max1)
+                {
+                    max2 = a[i];
+                }
+            }
+
+            if (max2 == int.MinValue)
+            {
+                throw new InvalidOperationException("Không tồn tại số lớn thứ hai (do tất cả phần tử giống nhau).");
+            }
+
+            return max2;
+        }
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            int n;
+            while (true)
+            {
+                Console.Write("Nhập n: ");
+                string? input = Console.ReadLine();
+
+                if (int.TryParse(input, out n))
+                    break;
+                else
+                    Console.WriteLine("Vui lòng nhập một số nguyên.");
+            }
+            //Khai báo và khởi tạo mảng số nguyên có n phần tử
+            int[] a = new int[n];
+            NhapMang(a, n);
+            try
+            {
+                int soLonThuHai = TimSoLonThuHai(a, n);
+                Console.WriteLine($"Số lớn thứ 2 trong mảng là: {soLonThuHai}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Lỗi: {ex.Message}");
+            }
+
+        }
+    }
+
+//Bai 5: Viết hàm hoán vị 2 số nguyên a và b nhập từ bàn phím
+    class Program5
+    {
+        public static void HoanVi(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Nhập số nguyên a: ");
+            int a = int.Parse(Console.ReadLine()!);
+
+            Console.Write("Nhập số nguyên b: ");
+            int b = int.Parse(Console.ReadLine()!);
+
+            Console.WriteLine($"Trước khi hoán vị: a = {a}, b = {b}");
+
+            HoanVi(ref a, ref b);
+
+            Console.WriteLine($"Sau khi hoán vị: a = {a}, b = {b}");
+
+        }
+    }
+
+ //Bai 6: Viết hàm sắp xếp mảng số thực n phần tử nhập từ bàn phím theo chiều tăng dần
+    class Program6
+    {
+        public static void NhapMang(double[] a, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                int value;
+                while (true)
+                {
+                    Console.Write($"a[{i}]: ");
+                    string? input = Console.ReadLine();
+
+                    if (int.TryParse(input, out value))
+                    {
+                        a[i] = value;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Giá trị không hợp lệ. Vui lòng nhập số nguyên.");
+                    }
+                }
+            }
+        }
+        public static void SapXepTangDan(double[] a, int n)
+        {
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (a[i] > a[j])
+                    {
+                        double temp = a[i];
+                        a[i] = a[j];
+                        a[j] = temp;
+                    }
+                }
+            }
+        }
+        public static void XuatMang(double[] a, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"{a[i]} ");
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            int n;
+            while (true)
+            {
+                Console.Write("Nhập số lượng phần tử n: ");
+                string? input = Console.ReadLine();
+                if (int.TryParse(input, out n) && n > 0)
+                    break;
+                else
+                    Console.WriteLine("Vui lòng nhập số nguyên dương.");
+            }
+
+            double[] a = new double[n];
+            NhapMang(a, n);
+
+            Console.WriteLine("Mảng trước khi sắp xếp:");
+            XuatMang(a, n);
+
+            SapXepTangDan(a, n);
+
+            Console.WriteLine("Mảng sau khi sắp xếp tăng dần:");
+            XuatMang(a, n);
+        }
+
+
+    }
+
+}
