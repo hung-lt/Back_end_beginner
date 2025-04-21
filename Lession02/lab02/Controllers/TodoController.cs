e79e64ae3512237e23144e314a087c8f823ab0b7
Hưng <92345487+hung-lt@users.noreply.github.com>
2025-04-21 20:59:47 +0700

    Add files via upload

---
+++ Lession02/lab02/Controllers/TodoController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class TodoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Add()
+        {
+            return Add();
+        }
+        public IActionResult Edit()
+        {
+            return Edit();
+        }
+    }
+}
