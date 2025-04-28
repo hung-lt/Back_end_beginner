47af6b5d37b357f70af47539e9e02758ee14b610
Hưng <92345487+hung-lt@users.noreply.github.com>
2025-04-28 22:20:04 +0700

    Delete ReceivingParameter directory

--- ReceivingParameter/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ReceivingParameter.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
